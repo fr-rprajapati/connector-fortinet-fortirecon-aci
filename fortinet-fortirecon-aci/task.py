43c6cc217711d519b21b3361e59302259412ba54
rprajapati <rprajapati@fortinet.com>
2025-09-25 16:29:34 +0530

    feat(77): Operation tasks issue fixed

--- fortinet-fortirecon-aci/task.py
+++ fortinet-fortirecon-aci/task.py
@@ -11,12 +11,12 @@ from .make_rest_api_call import MakeRestApiCall
 def create_task(config, params):
     MK = MakeRestApiCall(config=config)
     endpoint = "/security-orchestration/{org_id}/tasks"
-    response = MK.make_request(endpoint=endpoint, method="POST", params=params)
+    response = MK.make_request(endpoint=endpoint, method="POST", json_data=params)
     return response
 
 
 def update_task(config, params):
     MK = MakeRestApiCall(config=config)
     endpoint = "/security-orchestration/{org_id}"+"/tasks/{0}".format(params.pop("task_id"))
-    response = MK.make_request(endpoint=endpoint, method="PATCH", params=params)
+    response = MK.make_request(endpoint=endpoint, method="PATCH", json_data=params)
     return response
